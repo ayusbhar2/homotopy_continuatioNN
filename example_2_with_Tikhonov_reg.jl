7aba6737e4f59563d777eb7ccff1183828560145
Ayush Bharadwaj <ayushbharadwaj@Ayushs-MacBook-Pro.local>
2022-03-25 17:12:20 -0700

    output formatting

--- example_2_with_Tikhonov_reg.jl
+++ example_2_with_Tikhonov_reg.jl
@@ -19,7 +19,7 @@ a = 0; b= 1;
 
 H = 1; dx = 2; dy = 2; m = 5
 
-runs = 1
+runs = 2
 
 # variables
 @var α1, α2, γ1, γ2
@@ -31,35 +31,35 @@ W2 = [γ1; γ2]		# output layer with two neurons
 
 W = W2*W1
 
-
-# Tikhonov regularization constants
-U = Uniform(a, b)	# used for constructing the Tikhonov matrices
-Λ₁ = utils.generate_Tikhonov_matrix(U, size(W1))
-Λ₂ = utils.generate_Tikhonov_matrix(U, size(W2))
-
-
-# Compute regularized gratiend polynomials
-∂_L_W1 = transpose(W2) * (W * X * transpose(X) - Z * transpose(X)) + Λ₁ .* W1	# gradient matrix w.r.t. layer 1
-∂_L_W2 = (W * X * transpose(X) - Z * transpose(X)) * transpose(W1)	+ Λ₂ .* W2		# gradient matrix w.r.t. layer 2
-
-f_α1 = ∂_L_W1[1,1]
-f_α2 = ∂_L_W1[1,2]
-f_γ1 = ∂_L_W2[1,1]
-f_γ2 = ∂_L_W2[2,1]
-
-∇L = System([f_α1, f_α2, f_γ1, f_γ2], [α1, α2, γ1, γ2])
-
-result = solve(∇L)
-
-cbb = utils.get_CBB(∇L)
-n_dm = convert(Int64, trunc(utils.get_N_DM(1, nvariables(∇L))))
-n_r = utils.get_N_R(result)
-n_c = utils.get_N_C(result)
+for run = 1:runs
+	# Tikhonov regularization constants
+	U = Uniform(a, b)	# used for constructing the Tikhonov matrices
+	Λ₁ = utils.generate_Tikhonov_matrix(U, size(W1))
+	Λ₂ = utils.generate_Tikhonov_matrix(U, size(W2))
 
 
-f = open("log.txt", "w")
-write(f, string("H \t dx \t dy \t m \t a \t b \t CBB \t N_DM \t N_R \t N_C\n"))
-write(f, string(H, "\t", dx, "\t", dy, "\t", m, "\t", a, "\t", b, "\t", cbb, "\t", n_dm, "\t", n_r, "\t", n_c, "\n"))
-close(f)
+	# Compute regularized gratiend polynomials
+	∂_L_W1 = transpose(W2) * (W * X * transpose(X) - Z * transpose(X)) + Λ₁ .* W1	# gradient matrix w.r.t. layer 1
+	∂_L_W2 = (W * X * transpose(X) - Z * transpose(X)) * transpose(W1)	+ Λ₂ .* W2		# gradient matrix w.r.t. layer 2
 
+	f_α1 = ∂_L_W1[1,1]
+	f_α2 = ∂_L_W1[1,2]
+	f_γ1 = ∂_L_W2[1,1]
+	f_γ2 = ∂_L_W2[2,1]
+
+	∇L = System([f_α1, f_α2, f_γ1, f_γ2], [α1, α2, γ1, γ2])
+
+	result = solve(∇L)
+
+	cbb = utils.get_CBB(∇L)
+	n_dm = convert(Int64, trunc(utils.get_N_DM(1, nvariables(∇L))))
+	n_r = utils.get_N_R(result)
+	n_c = utils.get_N_C(result)
+
+
+	f = open("log.txt", "a")
+	write(f, string("No. \t H \t dx \t dy \t m \t a \t b \t CBB \t N_DM \t N_R \t N_C\n"))
+	write(f, string(run, "\t", H, "\t", dx, "\t", dy, "\t", m, "\t", a, "\t", b, "\t", cbb, "\t", n_dm, "\t", n_r, "\t", n_c, "\n"))
+	close(f)
+end
 
